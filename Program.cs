1afa8228f685f8e2dbb9e1802191712c0f69a225
klai-cs <koeyl.ca@gamil.com>
2021-07-27 07:30:40 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP311_s2021_ClassDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
